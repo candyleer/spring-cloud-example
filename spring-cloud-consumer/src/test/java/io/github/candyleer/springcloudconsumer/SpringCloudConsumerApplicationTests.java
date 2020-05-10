fc2f90c822311e27ba055c817b9f2ee0deb2cc46
candyleer <lican@apache.org>
2020-05-10 19:09:12 +0800

    init

---
+++ spring-cloud-consumer/src/test/java/io/github/candyleer/springcloudconsumer/SpringCloudConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.candyleer.springcloudconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
